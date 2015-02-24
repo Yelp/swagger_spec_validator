33861919e667f93263f72e30c3d3cd7e459e0773
Prateek Agarwal <agarwal@yelp.com>
2015-02-23 16:30:05 -0800

    Fixes #6 and #7

--- swagger_spec_validator/validator20.py
+++ swagger_spec_validator/validator20.py
@@ -5,32 +5,34 @@ import urllib2
 
 from swagger_spec_validator.common import (SwaggerValidationError,
                                            TIMEOUT_SEC,
-                                           validate_json)
+                                           validate_json,
+                                           wrap_exception)
 
 log = logging.getLogger(__name__)
 
 
+@wrap_exception
 def validate_spec_url(spec_url):
     """Validates a Swagger 2.0 API Specification at the given URL.
 
     :param spec_url: the URL of the api-docs.
     :returns: `None` in case of success, otherwise raises an exception.
     :raises: :py:class:`swagger_spec_validator.SwaggerValidationError`
-    :raises: :py:class:`jsonschema.exceptions.ValidationError`
     """
     log.info('Validating %s' % spec_url)
     spec_json = json.load(urllib2.urlopen(spec_url, timeout=TIMEOUT_SEC))
     validate_spec(spec_json)
 
 
-def validate_spec(spec_json):
+def validate_spec(spec_json, _spec_url=None):
     """Validates a Swagger 2.0 API Specification given a Swagger Spec.
 
     :param spec_json: the json dict of the swagger spec.
     :type spec_json: dict
+    :param _spec_url: url serving the spec json (currently not used)
+    :type _spec_url: string
     :returns: `None` in case of success, otherwise raises an exception.
     :raises: :py:class:`swagger_spec_validator.SwaggerValidationError`
-    :raises: :py:class:`jsonschema.exceptions.ValidationError`
     """
     validate_json(spec_json, 'schemas/v2.0/schema.json')
 
--- tests/validator20/validate_spec_url_test.py
+++ tests/validator20/validate_spec_url_test.py
@@ -1,6 +1,8 @@
 import mock
+import pytest
 import StringIO
 
+from swagger_spec_validator.common import SwaggerValidationError
 from swagger_spec_validator.validator20 import validate_spec_url
 
 
@@ -12,3 +14,10 @@ def test_success(petstore_contents):
         validate_spec_url('http://localhost/api-docs')
         mock_urlopen.assert_has_calls([
             mock.call('http://localhost/api-docs', timeout=1)])
+
+
+def test_raise_SwaggerValidationError_on_urlopen_error():
+    with pytest.raises(SwaggerValidationError) as excinfo:
+        validate_spec_url('http://foo')
+    assert ('<urlopen error [Errno -2] Name or service not known>'
+            in str(excinfo.value))
