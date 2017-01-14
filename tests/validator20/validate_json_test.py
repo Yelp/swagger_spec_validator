d2f084b78fe3d55570947080f5d9be800bd3b14d
Diogo Dutra <dutradda@gmail.com>
2017-01-14 01:40:40 -0200

    Update validate_json_test.py

--- tests/validator20/validate_json_test.py
+++ tests/validator20/validate_json_test.py
@@ -17,10 +17,10 @@ def test_success():
     my_dir = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(my_dir, '../data/v2.0/petstore.json')) as f:
         petstore_spec = json.load(f)
-    validate_json(petstore_spec, 'schemas/v2.0/schema.json')
+    validate_json(petstore_spec, 'swagger_spec_validator', 'schemas/v2.0/schema.json')
 
 
 def test_failure():
     with pytest.raises(SwaggerValidationError) as excinfo:
-        validate_json({}, 'schemas/v2.0/schema.json')
+        validate_json({}, 'swagger_spec_validator', 'schemas/v2.0/schema.json')
     assert "'swagger' is a required property" in str(excinfo.value)
