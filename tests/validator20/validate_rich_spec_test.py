7f18e611729b07fb993a3662dbf4fae3333e019c
Samuele Maci <maci@yelp.com>
2017-03-12 09:09:51 +0100

    Add requirements-dev and pre-commit hook Defined setup.py classifications Travis tests with python 3.5 and 3.6

--- tests/validator20/validate_rich_spec_test.py
+++ tests/validator20/validate_rich_spec_test.py
@@ -16,8 +16,7 @@ def test_failure_on_duplicate_api_parameters(swagger_spec):
     swagger_spec['paths']['/pet']['parameters'] = [param1, param2]
     with pytest.raises(SwaggerValidationError) as exc_info:
         validate_spec(swagger_spec)
-    assert ("Duplicate param found with (name, in): ('foo', 'query')"
-            in str(exc_info.value))
+    assert ("Duplicate param found with (name, in): {}".format(("foo", "query")) in str(exc_info.value))
 
 
 def test_failure_on_duplicate_operation_parameters(swagger_spec):
@@ -26,8 +25,7 @@ def test_failure_on_duplicate_operation_parameters(swagger_spec):
     swagger_spec['paths']['/pet']['post']['parameters'].extend([param1, param2])
     with pytest.raises(SwaggerValidationError) as exc_info:
         validate_spec(swagger_spec)
-    assert ("Duplicate param found with (name, in): ('foo', 'query')"
-            in str(exc_info.value))
+    assert ("Duplicate param found with (name, in): {}".format(("foo", "query")) in str(exc_info.value))
 
 
 def test_failure_on_unresolvable_path_parameter(swagger_spec):
@@ -48,12 +46,9 @@ def test_failure_on_unresolvable_ref_of_props_required_list(swagger_spec):
     swagger_spec['definitions']['Pet']['required'].append('bla')
     with pytest.raises(SwaggerValidationError) as exc_info:
         validate_spec(swagger_spec)
-    assert ("Required list has properties not defined: ['bla']"
-            in str(exc_info.value))
+    assert ("Required list has properties not defined: {}".format(['bla']) in str(exc_info.value))
 
 
-# TODO: validate definitions of references made by $ref, commented them for now.
-"""
 def test_failure_on_unresolvable_model_reference_from_model(swagger_spec):
     swagger_spec['definitions']['Pet']['properties']['category']['$ref'] = '_'
     with pytest.raises(SwaggerValidationError):
@@ -72,7 +67,6 @@ def test_failure_on_unresolvable_model_reference_from_resp(swagger_spec):
     resp['200']['schema']['items']['$ref'] = '#/definitions/bla'
     with pytest.raises(SwaggerValidationError):
         validate_spec(swagger_spec)
-"""
 
 # TODO: Add warning validations for unused models, path parameter & responses
 # TODO: Add validations for cyclic model definitions
