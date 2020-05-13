23133107347676a1229abe36bbac74c041d30ed1
Bryce <gpg-2019-08-19@brycedrennan.com>
2020-05-12 21:45:07 -0700

    tests: identify valid url error

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ def is_urlopen_error(exception):
         urlopen_error_str in str(exception)
         for urlopen_error_str in {
             '<urlopen error [Errno -2] Name or service not known>',
+            '<urlopen error [Errno -3] Temporary failure in name resolution>',
             '<urlopen error [Errno 8] nodename nor servname provided, or not known',
             '<urlopen error [Errno -5] No address associated with hostname>',
             '<urlopen error [Errno 11001] getaddrinfo failed>',
