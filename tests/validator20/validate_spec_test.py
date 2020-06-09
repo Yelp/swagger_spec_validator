d0fe8053ea365abf23ca2d8b1927b8836158a703
Stephan Jaensch <sjaensch@yelp.com>
2020-06-09 13:17:31 +0200

    Fix: additionalProperties can be a boolean value

--- tests/validator20/validate_spec_test.py
+++ tests/validator20/validate_spec_test.py
@@ -427,8 +427,6 @@ def test_highlight_inconsistent_schema_object_validation(minimal_swagger_dict, s
 
 def test_additional_properties_validated(minimal_swagger_dict, default_checks_spec_dict):
     invalid_spec = {'type': 'object', 'required': ['foo']}
-    minimal_swagger_dict['definitions']['injected_definition'] = invalid_spec
-
     minimal_swagger_dict['definitions']['injected_definition'] = {
         'type': 'object', 'additionalProperties': invalid_spec
     }
@@ -437,10 +435,16 @@ def test_additional_properties_validated(minimal_swagger_dict, default_checks_sp
         validate_spec(minimal_swagger_dict)
 
 
+def test_additional_properties_bool(minimal_swagger_dict, default_checks_spec_dict):
+    minimal_swagger_dict['definitions']['injected_definition'] = {
+        'type': 'object', 'additionalProperties': False,
+    }
+
+    validate_spec(minimal_swagger_dict)
+
+
 def test_array_items_validated(minimal_swagger_dict, default_checks_spec_dict):
     invalid_spec = {'type': 'object', 'required': ['foo']}
-    minimal_swagger_dict['definitions']['injected_definition'] = invalid_spec
-
     minimal_swagger_dict['definitions']['injected_definition'] = {
         'type': 'array', 'items': invalid_spec
     }
